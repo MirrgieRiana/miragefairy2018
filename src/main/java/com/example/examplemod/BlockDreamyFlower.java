3e33e85fef870e1df215e69ae993e9677b86cc1f
Mirrgie Riana <tacticsrealize2@gmail.com>
2018-05-10 17:58:56 +0900

    update: dreamy_flower

--- src/main/java/com/example/examplemod/BlockDreamyFlower.java
+++ src/main/java/com/example/examplemod/BlockDreamyFlower.java
@@ -3,58 +3,157 @@ package com.example.examplemod;
 import java.util.Random;
 
 import net.minecraft.block.BlockBush;
+import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.properties.IProperty;
 import net.minecraft.block.properties.PropertyInteger;
 import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.init.Items;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.NonNullList;
+import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 
 public class BlockDreamyFlower extends BlockBush
 {
 
-	public static final PropertyInteger STAGE = PropertyInteger.create("stage", 0, 3);
-
 	public BlockDreamyFlower()
 	{
 		super(Material.GLASS);
+
+		// meta
 		setDefaultState(blockState.getBaseState()
-			.withProperty(STAGE, 3));
+			.withProperty(AGE, 0));
+
+		// style
+		setSoundType(SoundType.GLASS);
+
 	}
+
+	// meta
+
+	public static final PropertyInteger AGE = PropertyInteger.create("stage", 0, 3);
+
+	@Override
+	public int getMetaFromState(IBlockState state)
+	{
+		return state.getValue(AGE);
+	}
+
+	@Override
+	public IBlockState getStateFromMeta(int meta)
+	{
+		return getDefaultState().withProperty(AGE, meta);
+	}
+
+	@Override
+	protected BlockStateContainer createBlockState()
+	{
+		return new BlockStateContainer(this, new IProperty[] { AGE });
+	}
+
+	// style
+
+	public static final AxisAlignedBB AABB_STAGE0 = new AxisAlignedBB(5 / 16.0, 0 / 16.0, 5 / 16.0, 11 / 16.0, 5 / 16.0, 11 / 16.0);
+	public static final AxisAlignedBB AABB_STAGE1 = new AxisAlignedBB(2 / 16.0, 0 / 16.0, 2 / 16.0, 14 / 16.0, 12 / 16.0, 14 / 16.0);
+	public static final AxisAlignedBB AABB_STAGE2 = new AxisAlignedBB(2 / 16.0, 0 / 16.0, 2 / 16.0, 14 / 16.0, 16 / 16.0, 14 / 16.0);
+	public static final AxisAlignedBB AABB_STAGE3 = new AxisAlignedBB(2 / 16.0, 0 / 16.0, 2 / 16.0, 14 / 16.0, 16 / 16.0, 14 / 16.0);
+
+	@Override
+	public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos)
+	{
+		int stage = state.getValue(AGE);
+		if (stage == 0) return AABB_STAGE0;
+		if (stage == 0) return AABB_STAGE1;
+		if (stage == 0) return AABB_STAGE2;
+		if (stage == 0) return AABB_STAGE3;
+		return AABB_STAGE3;
+	}
+
+	// behaviour
 
 	@Override
 	public void updateTick(World worldIn, BlockPos pos, IBlockState state, Random rand)
 	{
 		super.updateTick(worldIn, pos, state, rand);
 
-		int stage = state.getValue(STAGE);
+		int stage = state.getValue(AGE);
 		if (stage < 3) {
-			worldIn.setBlockState(pos, getDefaultState().withProperty(STAGE, stage + 1), 2);
+			worldIn.setBlockState(pos, getDefaultState().withProperty(AGE, stage + 1), 2);
 		}
 
 	}
 
+	/**
+	 * Spawns this Block's drops into the World as EntityItems.
+	 */
 	@Override
-	public int getMetaFromState(IBlockState state)
+	@SuppressWarnings("unused")
+	public void dropBlockAsItemWithChance(World worldIn, BlockPos pos, IBlockState state, float chance, int fortune)
 	{
-		return state.getValue(STAGE);
+		super.dropBlockAsItemWithChance(worldIn, pos, state, chance, fortune);
+		if (false && !worldIn.isRemote) {
+			int i = 1;
+
+			if (state.getValue(AGE).intValue() >= 3) {
+				i = 2 + worldIn.rand.nextInt(3);
+
+				if (fortune > 0) {
+					i += worldIn.rand.nextInt(fortune + 1);
+				}
+			}
+
+			for (int j = 0; j < i; ++j) {
+				spawnAsEntity(worldIn, pos, new ItemStack(Items.NETHER_WART));
+			}
+		}
+	}
+
+	/**
+	 * Get the Item that this Block should drop when harvested.
+	 */
+	@Override
+	public Item getItemDropped(IBlockState state, Random rand, int fortune)
+	{
+		return Items.AIR;
+	}
+
+	/**
+	 * Returns the quantity of items to drop on block destruction.
+	 */
+	@Override
+	public int quantityDropped(Random random)
+	{
+		return 0;
 	}
 
 	@Override
-	public IBlockState getStateFromMeta(int meta)
+	public ItemStack getItem(World worldIn, BlockPos pos, IBlockState state)
 	{
-		return getDefaultState().withProperty(STAGE, meta);
+		return new ItemStack(Items.NETHER_WART);
 	}
 
 	@Override
-	protected BlockStateContainer createBlockState()
+	public void getDrops(net.minecraft.util.NonNullList<ItemStack> drops, net.minecraft.world.IBlockAccess world, BlockPos pos, IBlockState state, int fortune)
 	{
-		return new BlockStateContainer(this, new IProperty[] { STAGE });
+		Random rand = world instanceof World ? ((World) world).rand : new Random();
+		int count = 1;
+
+		if ((state.getValue(AGE)) >= 3) {
+			count = 2 + rand.nextInt(3) + (fortune > 0 ? rand.nextInt(fortune + 1) : 0);
+		}
+
+		for (int i = 0; i < count; i++) {
+			drops.add(new ItemStack(Items.NETHER_WART));
+		}
 	}
+
+	//
 
 	@Override
 	public void getSubBlocks(CreativeTabs itemIn, NonNullList<ItemStack> items)
--- src/main/java/com/example/examplemod/ExampleMod.java
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -2,15 +2,18 @@ package com.example.examplemod;
 
 import org.apache.logging.log4j.Logger;
 
-import net.minecraft.block.Block;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.creativetab.CreativeTabs;
-import net.minecraft.item.ItemBlock;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.registry.ForgeRegistries;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 @Mod(modid = ExampleMod.MODID, name = ExampleMod.NAME, version = ExampleMod.VERSION)
 public class ExampleMod
@@ -20,12 +23,12 @@ public class ExampleMod
 	public static final String NAME = "MirageFairy2018";
 	public static final String VERSION = "0.0.1";
 
-	private static Logger logger;
+	public static Logger logger;
 
 	public static CreativeTabs creativeTab;
 
-	public static Block blockDreamyFlower;
-	public static ItemBlock itemDreamyFlower;
+	public static BlockDreamyFlower blockDreamyFlower;
+	public static Item itemDreamyFlowerSeeds;
 
 	@EventHandler
 	public void preInit(FMLPreInitializationEvent event)
@@ -35,9 +38,10 @@ public class ExampleMod
 
 		creativeTab = new CreativeTabs("MirageFairy2018") {
 			@Override
+			@SideOnly(Side.CLIENT)
 			public ItemStack getTabIconItem()
 			{
-				return new ItemStack(itemDreamyFlower);
+				return new ItemStack(itemDreamyFlowerSeeds);
 			}
 		};
 
@@ -46,11 +50,12 @@ public class ExampleMod
 		blockDreamyFlower.setCreativeTab(creativeTab);
 		ForgeRegistries.BLOCKS.register(blockDreamyFlower);
 
-		itemDreamyFlower = new ItemBlock(blockDreamyFlower);
-		itemDreamyFlower.setRegistryName(MODID, "dreamy_flower");
-		itemDreamyFlower.setCreativeTab(creativeTab);
-		itemDreamyFlower.setUnlocalizedName("dreamy_flower");
-		ForgeRegistries.ITEMS.register(itemDreamyFlower);
+		itemDreamyFlowerSeeds = new ItemSeedDreamyFlower();
+		itemDreamyFlowerSeeds.setRegistryName(MODID, "dreamy_flower_seeds");
+		itemDreamyFlowerSeeds.setCreativeTab(creativeTab);
+		itemDreamyFlowerSeeds.setUnlocalizedName("dreamy_flower_seeds");
+		ForgeRegistries.ITEMS.register(itemDreamyFlowerSeeds);
+		ModelLoader.setCustomModelResourceLocation(itemDreamyFlowerSeeds, 0, new ModelResourceLocation(MODID + ":dreamy_flower_seeds"));
 
 	}
 
