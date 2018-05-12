bbd23dd6d7202cc98c5f9b449dedcc187aad3d8c
Mirrgie Riana <tacticsrealize2@gmail.com>
2018-05-12 22:58:24 +0900

    fix

--- src/main/java/miragefairy2018/SubItemMatrix.java
+++ src/main/java/miragefairy2018/SubItemMatrix.java
@@ -16,7 +16,7 @@ public class SubItemMatrix extends SubItem
 		super(
 			shape.id * shapeOffset + material.id,
 			shape.name + material.name,
-			material.resourceName + "_" + shape.resourceName);
+			material.resourceName + (shape.resourceName.isEmpty() ? "" : "_" + shape.resourceName));
 		this.shape = shape;
 		this.material = material;
 	}
