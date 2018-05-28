6356167d326ae3f648a46a6191f8cec8b9eafc11
Mirrgie Riana <tacticsrealize2@gmail.com>
2018-05-28 23:13:38 +0900

    closed #50

--- src/main/java/miragefairy2018/mod/ore/ModuleOre.java
+++ src/main/java/miragefairy2018/mod/ore/ModuleOre.java
@@ -1,5 +1,6 @@
 package miragefairy2018.mod.ore;
 
+import miragefairy2018.lib.util.Util;
 import miragefairy2018.mod.EventRegistryMod;
 import miragefairy2018.mod.ModMirageFairy2018;
 import miragefairy2018.mod.ModuleMain;
@@ -7,7 +8,12 @@ import miragefairy2018.mod.material.ModuleMaterial;
 import net.minecraft.client.renderer.block.statemap.StateMap;
 import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
+import net.minecraft.world.gen.feature.WorldGenMinable;
+import net.minecraft.world.gen.feature.WorldGenerator;
 import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.terraingen.OreGenEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.registry.ForgeRegistries;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraftforge.oredict.OreDictionary;
@@ -61,6 +67,39 @@ public class ModuleOre
 			// 製錬レシピ
 			GameRegistry.addSmelting(blockOreMiragium, ModuleMaterial.dustMiragium.getItemStack(), 5);
 			GameRegistry.addSmelting(blockOreMiragonite, ModuleMaterial.gemMiragonite.getItemStack(), 0.7f);
+
+		});
+		erMod.decorate.register(() -> {
+
+			// 地形生成
+			MinecraftForge.ORE_GEN_BUS.register(new Object() {
+
+				private WorldGenMinable worldGenMiragium = new WorldGenMinable(blockOre.getState(BlockOre.EnumType.MIRAGIUM), 4);
+				private WorldGenMinable worldGenMiragonite = new WorldGenMinable(blockOre.getState(BlockOre.EnumType.MIRAGONITE), 9);
+
+				@SubscribeEvent
+				public void accept(OreGenEvent.Post event)
+				{
+
+					// iron = size:9;count:20;minHeight:0;maxHeight:64; c/16=5
+					// gold = size:9;count:2;minHeight:0;maxHeight:32;  c/16=1
+					genStandardOre(event, 0.5, worldGenMiragium, 64, 255);
+					genStandardOre(event, 2, worldGenMiragonite, 32, 255);
+
+				}
+
+				private void genStandardOre(OreGenEvent.Post event, double countPerY16, WorldGenerator generator, int minHeightInclusive, int maxHeightExclusive)
+				{
+					int count = Util.randomInt(countPerY16 * (maxHeightExclusive - minHeightInclusive) / 16.0, event.getRand());
+					for (int j = 0; j < count; ++j) {
+						generator.generate(event.getWorld(), event.getRand(), event.getPos().add(
+							event.getRand().nextInt(16),
+							event.getRand().nextInt(maxHeightExclusive - minHeightInclusive) + minHeightInclusive,
+							event.getRand().nextInt(16)));
+					}
+				}
+
+			});
 
 		});
 	}
